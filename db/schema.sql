4f367e9eef86eaf7a80c727c2efddb7597f6bf59
surielsa <surielsa@gmail.com>
2019-08-06 20:48:08 -0400

    update

--- db/schema.sql
+++ db/schema.sql
@@ -1,27 +1,20 @@
-DROP DATABASE IF EXISTS exotic_Cars_DB;
+DROP DATABASE IF EXISTS tools_db;
+CREATE DATABASE tools_db;
 
-CREATE DATABASE exotic_Cars_DB;
-USE exotic_Cars_DB;
+USE tools_db;
 
-CREATE TABLE cars (
-    id INT NOT NULL AUTO_INCREMENT,
-    car_Year YEAR NOT NULL,
-    car_Make VARCHAR(255) NOT NULL,
-    car_Model VARCHAR(255) NOT NULL,
-    transmission_Type VARCHAR(255) NOT NULL,
-    start_DA DATE,
-    end_DA DATE,
-    car_Miles INT NOT NULL,
-    car_Availabilty BOOLEAN DEFAULT true,
-    car_Rates INT,
-    car_Condition VARCHAR(255),
-    car_ImageURL VARCHAR(255),
-    car_Spare1 VARCHAR(255),
-    car_Spare2 VARCHAR(255),
-    car_Spare3 VARCHAR(255),
-    car_Spare4 VARCHAR(255),
-    car_Spare5 INT,
-    car_Spare6 INT,
-    car_Spare7 INT,
-    PRIMARY KEY (id)
+CREATE TABLE tools (
+  item_id INT AUTO_INCREMENT NOT NULL,
+  tool_name VARCHAR(45) NOT NULL,
+  department_name VARCHAR(45) NOT NULL,
+  deposit_amount DECIMAL(10,2) NOT NULL,
+  price_perhour DECIMAL(10,2) NOT NULL,
+  price_per4hour DECIMAL(10,2) NOT NULL,
+  price_perday DECIMAL(10,2) NOT NULL,
+  price_perweek DECIMAL(10,2) NOT NULL,
+  tool_availability INT(10) NOT NULL,
+  primary key(item_id)
 );
+SELECT * FROM tools;
+
+
